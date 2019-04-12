916ded3b6c2b5278ba89d0ebe4332b2c1f5c7a46
xjd <xjd811001@163.com>
2019-04-12 15:30:09 +0800

    create

---
+++ testRunner.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+from baseLib import HTMLTestRunner
+import BeautifulReport
+from testSuite.singleTransferTest import transfer
+
+current_path = os.getcwd()
+case_path = os.path.join(current_path, 'testSuite')
+report_path = os.path.join(current_path, 'report')
+
+def all_case():
+    discover = unittest.defaultTestLoader.discover(case_path, pattern = '*test*.py')
+    # discover = unittest.defaultTestLoader.discover(case_path, pattern='practice1.py')
+    return discover
+if __name__=='__main__':
+    # result_path = report_path = os.path.join(report_path, 'report.html')
+    # fp = open(result_path, 'wb')
+    # runner = HTMLTestRunner.HTMLTestRunner(stream = fp, title ='测试报告', description='用例执行情况')
+    # runner.run(all_case())
+    # fp.close()
+
+    # testSuite = unittest.TestSuite()
+    # testSuite.addTest(unittest.makeSuite(transfer))
+    # run = BeautifulReport.BeautifulReport(testSuite)
+    # run.report(filename='test', description='测试用例', log_path=report_path)
+
+    run = BeautifulReport.BeautifulReport(all_case())
+    run.report(filename='test', description='测试用例', log_path=report_path)
+
+    # unittest.main()
