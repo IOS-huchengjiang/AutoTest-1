b214ac44f966811b75e7feac49f971288c10561e
xjd <xjd811001@163.com>
2019-05-08 14:17:56 +0800

    框架调整后提交

--- functionTest/baseLib_oprationManagerAPI_authentication_test.py
+++
@@ -1,8 +0,0 @@
-from baseLib.operationManagerAPI import authentication
-import unittest
-from configuration import const
-
-class authenticationTest(unittest.TestCase):
-    def test001(self):
-        returnTuple = authentication.login(const._global_configuration().optionManagerOperator)
-        print(returnTuple)
